b13640d65d08ca7f0eaeae9b81ba57937ce7ea15
Muhammad Haikal <realityinaship@gmail.com>
2021-04-22 15:13:24 +0700

    Added comments

--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@ from main import *
 class Dino:
 
     def __init__(self):
+        #Generating attributes/properties.
         self.DINO_IMG = "@"
         self.OBSTACLE_IMG = "|"
         self.SPEEDS = {
@@ -18,10 +19,12 @@ class Dino:
             self.highscore = pickle.load(open('Assets/highscore.pkl', 'rb'))
         except EOFError:
             self.highscore = 0
+        #Generate a road with a length of 80.
         self.road = ['_' for _ in range(80)]
         self.flag = True
 
     def screen_display(self, type=None):
+        #Displaying jump, dead, and normal scene to the screen.
         CLEAR()
         road_display = ''.join(self.road[:50])
         tab_score = '\t'*2
@@ -37,10 +40,14 @@ class Dino:
             print(f'\n\n\n\n\n  {tab_road}  {road_display}')
 
     def jump_handler(self):
+        #If the dinosaur jump, we're gonna fill the current position to a line
+        #And put the dinosaur on the top
         self.road[1] = '_'
         self.screen_display('jump')
 
     def speed_obstacle_handler(self):
+        #Check if the current score matching up new speed and obstacle count.
+        #If it's true, we're gonna add up the number of speed and obstacles.
         try:
             self.speed_count = self.SPEEDS[self.score][0]
             self.obstacle_count = self.SPEEDS[self.score][1]
@@ -48,31 +55,39 @@ class Dino:
             pass
                 
     def obstacle_generator(self):
+        #Generate obstacles in range 40 to 80 with 6 steps.
         for _ in range(self.obstacle_count):
             random_obstacle = choice([i for i in range(40,80,6)])
             self.road[random_obstacle] = self.OBSTACLE_IMG
 
     def score_handler(self):
+        #Adding up current score by 1 every 0.01 seconds.
         sleep(0.01)
         self.score += 1
 
     def death_handler(self):
+        #If the current score greater than the highscore, change the highscore.
         if self.score > self.highscore:
             pickle.dump(self.score, open('Assets/highscore.pkl', 'wb'))
         
+        #Printing dead scene to the screen.
         self.screen_display('dead')
 
     def obstacle_handler(self):
         obstacles_indices = [index for index,obs in enumerate(self.road) if obs == self.OBSTACLE_IMG]
         if len(obstacles_indices):
             for i in obstacles_indices:
+                #Check if the next element in the road is not a cactus.
                 if self.road[i-1] != self.DINO_IMG:
+                    #Deleting a cactus that reaches the end of the road.
                     if self.road[0] == self.OBSTACLE_IMG:
                         del obstacles_indices[0]
                         self.road[0] = '_'
+                    #Otherwise we're gonna fill it with road and generate new cactuses.
                     else:
                         self.road[i] = '_'
                         self.road[i-1] = self.OBSTACLE_IMG
+                #If the next element is a cactus, the game is over. Otherwise, fill it with new road.
                 else:
                     if self.road[1] == self.DINO_IMG: 
                         self.death_handler()
@@ -83,6 +98,7 @@ class Dino:
             self.obstacle_generator()
 
     def reset_entity(self):
+        #Reseting all game's entity to it's default value.
         self.flag = True
         self.speed_count = 0.07
         self.obstacle_count = 2
@@ -94,16 +110,19 @@ class Dino:
         while self.flag:
             self.road[1] = self.DINO_IMG
 
+            #Handling dinosaur's movement with keyboard.
             if keyboard.is_pressed('up') or keyboard.is_pressed('space'):
                 self.jump_handler()
             else:
                 self.screen_display()
 
-            self.obstacle_handler()
-            self.speed_obstacle_handler()
+            #Handling obstacles, speed, and score.
+            self.obstacle_handler() 
+            self.speed_obstacle_handler() 
             self.score_handler()
             sleep(self.speed_count)
 
+        #Check if user wants to play again.
         user_input = input("\n\t\t\t Play Again? (y/n): ").lower()
         if 'y' in user_input:
             self.reset_entity()
--- main.py
+++ main.py
@@ -8,6 +8,9 @@ CLEAR = lambda: print("\033[H\033[J")
 RESIZE = lambda: system('MODE 70, 28')
 
 def main():
+	'''Initialize object for 
+	Dino class and start the game'''
+
     game = Dino()
     RESIZE()
     game.start()
