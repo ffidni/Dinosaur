1308ea88e8b645609024bd2570f9768629569bd3
Muhammad Haikal <realityinaship@gmail.com>
2021-04-22 17:04:44 +0700

    Changing main's comment

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ CLEAR = lambda: print("\033[H\033[J")
 RESIZE = lambda: system('MODE 70, 28')
 
 def main():
-	'''Initialize object for 
-	Dino class and start the game'''
-
-    game = Dino()
+    #Initialize object 'game' to dino class and starting the game
     RESIZE()
+    game = Dino()
     game.start()
 
 if __name__ == '__main__':
